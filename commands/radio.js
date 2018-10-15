c49c81211387a72fd15fce14252cceb2f9ea4466
13ace37 <Ace_Graphics@outlook.de>
2018-10-15 20:06:44 +0200

    added .radio command || added ILoveRadio stream to .radio play
    Todo : add other streams

--- commands/radio.js
+++ commands/radio.js
@@ -1,8 +1,60 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, prefix) => {
-    
-    message.channel.send(`Yes it's working ${message.author}`);
+    var channel = "undefined";
+    if (!args[0]) {
+        return message.channel.send('Usage : `.radio play/stop/info`');
+    };
+    if (args[0].toLowerCase() == "play") {
+        if (message.member.voiceChannel) {
+            message.member.voiceChannel.join()
+                .then(connection => { 
+                    channel = message.member.voiceChannel;
+                    const dispatcher = connection.playStream('http://stream01.iloveradio.de/iloveradio1.mp3')
+                    message.channel.send("🎵 Now playing ILoveRadio 🎵");
+                    message.delete(10);
+                    dispatcher.on("end", end => {
+                        message.member.voiceChannel.leave();
+                    });
+                })
+                .catch(console.log);
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
+    if (args[0].toLowerCase() == "stop") {
+        try{
+            return message.member.voiceChannel.leave();
+        } catch (err){
+            message.channel.send("Oops!");
+        }
+            
+    }
+    if (args[0].toLowerCase() == "info") {
+        let stream = "http://stream01.iloveradio.de/iloveradio1.mp3";
+        internetradio.getStationInfo(stream, function (error, station) {
+            let radio = station.headers["icy-name"];
+            let title = station.title;
+            let track = title.split("-");
+            let trackName = track[1];
+            let trackAuthor = track[0];
+            if (channel != "undefined") {
+                let embed = new Discord.RichEmbed()
+                .setAuthor(`❌ **Not Playing** ❌`)
+                .setColor('FF0000');
+                return message.channel.send(embed);
+            }
+
+            let embed = new Discord.RichEmbed()
+                .setAuthor(`🎵 **${radio}** 🎵`)
+                .setColor('FF0000')
+                .setDescription(`
+                \`🎵\` **Song name :**  \`${trackName}\`
+                \`🎤\` **Author(s) :**  \`${trackAuthor}\`
+                `);
+            return message.channel.send(embed);
+        });
+    }
 
 };
 
