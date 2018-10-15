41709cbb9e8fce68cfa1349e40e1afa1f4e4e56c
13ace37 <Ace_Graphics@outlook.de>
2018-10-15 18:30:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const config = require("./config/bot.json");
 const Discord = require("discord.js"); 
 const fs = require("fs"); 
 
-const moment = require("moment");
-require("moment-duration-format");
-
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
