30ce9dba7f4bde522e11bcc2ded314c166bff6dd
13ace37 <Ace_Graphics@outlook.de>
2018-10-15 22:40:28 +0200

    added / fixed module node-internet-radio

--- commands/radio.js
+++ commands/radio.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const internetradio = require('node-internet-radio');
 
 module.exports.run = async (bot, message, args, prefix) => {
     var channel = "undefined";
